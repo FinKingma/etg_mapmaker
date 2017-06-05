be522fbbf22629bca31463089554cecde6139b10
Fin Kingma <Kingma@zorgdomein.nl>
2017-06-05 12:42:42 +0200

    .

--- retrieve-broker-url.sh
+++ retrieve-broker-url.sh
@@ -1,5 +1,5 @@
 aws configure set default.region us-east-1
 pbport=$(aws ecs describe-task-definition --task-definition pactbroker-family --query 'taskDefinition.containerDefinitions[0].portMappings[0].hostPort')
 pburl=$(aws ec2 describe-instances --instance-id i-0dc83ba1d1519c715 --query "Reservations[0].Instances[0].PublicIpAddress" | tr -d '"')
-echo pactbroker located on: \'$pactbrokerurl\'
-export pactbrokerurl=http://$pburl:$pbport
+export PACTBROKERURL=http://$pburl:$pbport
+echo pactbroker located on: \'$PACTBROKERURL\'

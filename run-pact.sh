ad0a18de43d80f4317451d0e08fb5fc43d47d00a
Fin Kingma <Kingma@zorgdomein.nl>
2017-06-05 12:49:17 +0200

    .

--- run-pact.sh
+++ run-pact.sh
@@ -1,5 +1,9 @@
+#get url from pact-broker
 aws configure set default.region us-east-1
 pbport=$(aws ecs describe-task-definition --task-definition pactbroker-family --query 'taskDefinition.containerDefinitions[0].portMappings[0].hostPort')
 pburl=$(aws ec2 describe-instances --instance-id i-0dc83ba1d1519c715 --query "Reservations[0].Instances[0].PublicIpAddress" | tr -d '"')
 export PACTBROKERURL=http://$pburl:$pbport
 echo pactbroker located on: \'$PACTBROKERURL\'
+
+#Run pact
+./node_modules/.bin/mocha pacts/mapmaker.spec.js

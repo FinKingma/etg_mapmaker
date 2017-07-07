915df34c67f31e87dc4659804a07390ee85571ed
Fin Kingma <Kingma@zorgdomein.nl>
2017-07-07 11:07:11 +0200

    fitnesse now saved in junit test map

--- fitnesse/runtest.sh
+++ fitnesse/runtest.sh
@@ -1,2 +1,2 @@
 
-java -jar fitnesse-standalone.jar -c "FrontPage?test&format=text" -l ~/ExploratoryGame/MapMakerApi/tmp/logs/ -v -b $CIRCLE_TEST_REPORTS/fitnesse/test-results.xml
+java -jar fitnesse-standalone.jar -c "FrontPage?test&format=text" -l ~/ExploratoryGame/MapMakerApi/tmp/logs/ -v -b $CIRCLE_TEST_REPORTS/junit/test-results.xml

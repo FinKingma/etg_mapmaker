91f775953c92ade431ee089b01b4b82eea07086e
Fin Kingma <Kingma@zorgdomein.nl>
2017-07-13 11:03:00 +0200

    added gor + cronjob + S3 stuff

---
+++ cronjob.js
@@ -0,0 +1,49 @@
+var CronJob = require('cron').CronJob;
+var fs = require("fs");
+var AWS = require('aws-sdk');
+var s3 = new AWS.S3();
+var myBucket = 'etg-bucket';
+var myKey = 'requests.gor';
+
+var job  = new CronJob({
+    //cronTime: '00 30 11 * * 1-5',
+    cronTime: '* * * * * 1-5',
+    onTick: function() {
+        console.log('discovering goreplay file...');
+        getGorFile(function(data) {
+            stuffS3With(data);
+        });
+        /*
+        * Runs every weekday (Monday through Friday)
+        * at 11:30:00 AM. It does not run on Saturday
+        * or Sunday.
+        */
+    },
+    start: false,
+    timeZone: 'America/Los_Angeles'
+});
+
+function getGorFile(callback) {
+    var data;
+    fs.readFile( "./" + "requests.gor","utf8", function (err, data) {
+        if (err) console.log('no requests.gor file found yet');
+        callback(data);
+    });
+}
+
+function stuffS3With(data) {
+    params = {Bucket: myBucket, Key: myKey, Body: data};
+    s3.putObject(params, function(err, data) {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log("Successfully uploaded data to myBucket/myKey");
+        }
+    });
+}
+
+module.exports = {
+    start: function() {
+        job.start();
+    }
+};
---
+++ gor.sh
@@ -0,0 +1,1 @@
+sudo ./gor --input-raw :3000 --input-raw-track-response --output-file ./requests.gor --output-file-append --output-file-flush-interval 1s
